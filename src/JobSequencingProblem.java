83d51b6e22f1075e45a254bd15d0222c29f29ca8
y22ming <olivia.ong22@gmail.com>
2022-09-02 04:26:18 +0800

    Update JobSequencingProblem.java

--- src/JobSequencingProblem.java
+++ src/JobSequencingProblem.java
@@ -7,44 +7,48 @@ import java.util.Iterator;
 
 public class JobSequencingProblem {
     public static void main(String[] args) {
-
         List<Job> jobList = new ArrayList<>();
         File file = new File("jobs.txt");
 
-        readFile(file, jobList);
+        if (readFile(file, jobList)) {
+            // 1. Greedy Method
+            System.out.println("Using Greedy Method:");
+            System.out.print("Final job sequence following maximum profit: ");
+            printJobs(GreedyMethod.sequenceJobs(jobList, jobList.size()));
 
-        // 1. Greedy Method
-        System.out.println("Using Greedy Method:");
-        System.out.print("Final job sequence following maximum profit: ");
-        printJobs(GreedyMethod.sequenceJobs(jobList, jobList.size()));
-
-        // 1. Naive Recursive Method
-        System.out.println("\n\nUsing Naive Recursive Method:");
-        System.out.print("Final job sequence following maximum profit: ");
-        printJobs(NaiveRecursiveMethod.sequenceJobs(jobList, jobList.size()));
+            // 2. Naive Recursive Method
+            System.out.println("\n\nUsing Naive Recursive Method:");
+            System.out.print("Final job sequence following maximum profit: ");
+            printJobs(NaiveRecursiveMethod.sequenceJobs(jobList, jobList.size()));
+        }
     }
 
-    public static void readFile(File file, List<Job> arraylist) {
+    public static boolean readFile(File file, List<Job> jobList) {
         try {
             Scanner in = new Scanner(file);
             String line;
             if (!in.hasNext()) {
                 System.out.println("File is empty!");
-                return;
+                in.close();
+                return false;
             }
             while (in.hasNext()) {
                 line = in.nextLine();
                 String[] words = line.split(",");
-                arraylist.add(new Assignment(words[0], Integer.parseInt(words[1]), Integer.parseInt(words[2]), Integer.parseInt(words[3])));
+                jobList.add(new Assignment(words[0], Integer.parseInt(words[1]), Integer.parseInt(words[2]),
+                        Integer.parseInt(words[3])));
             }
-        } catch (FileNotFoundException | IllegalArgumentException e) {
+            in.close();
+            return true;
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        return false;
     }
 
     public static void printJobs(List<Job> list) {
         Iterator<Job> iterator = list.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             Job theJob = iterator.next();
             System.out.print(theJob.getId() + " " + theJob.getProfit() + " marks, ");
         }
